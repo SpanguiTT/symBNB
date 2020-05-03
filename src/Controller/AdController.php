3fbf2462857fcad91e25af910f463b45544af5ab
Remi Spangaro <remi.spangaro@hotmail.fr>
2020-05-03 16:29:14 +0200

    Work in progress Dimanche 03/05/2020

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdController extends AbstractController
 {
@@ -26,18 +28,33 @@ class AdController extends AbstractController
     /**
      * Permet d'afficher une seule annonce
      *
-     * @Route("/ads/{slug}", name="ads_show")
+     * @Route("/ads/{slug}", name="ads_show_OLD")
      *
      * @param $slug
      * @param AdRepository $adRepository
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function show($slug, AdRepository $adRepository)
+    public function showOLD($slug, AdRepository $adRepository)
     {
         // Je récupère l'annonce qui correspond au slug
         //$ad = $adRepository->findOneBySlug($slug);
         $ad = $adRepository->findOneBy(array('slug' => $slug));
 
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @param Ad $ad
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(Ad $ad)
+    {
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
